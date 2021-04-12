f3bd24387f92838f68d0511341ee870abd549b1c
owenburton <owen.burton13@gmail.com>
2021-04-12 15:35:55 -0600

    Solve with one loop

--- squares_of_sorted_arr.go
+++ squares_of_sorted_arr.go
@@ -5,42 +5,31 @@ import "fmt"
 // Given an integer array nums sorted in non-decreasing order,
 // return an array of the squares of each number sorted in non-decreasing order.
 func sortedSquares(nums []int) []int {
-	r := 0
-	for r < len(nums) && nums[r] < 0 {
-		r++
-	}
-	l := r - 1
+	n := len(nums)
+	ans := make([]int, n)
 
-	answer := make([]int, 0)
+	l := 0
+	r := n - 1
 
-	for l >= 0 && r < len(nums) {
-		lSquare := nums[l] * nums[l]
-		rSquare := nums[r] * nums[r]
+	for i := n - 1; i >= 0; i-- {
+		lSqr := nums[l] * nums[l]
+		rSqr := nums[r] * nums[r]
 
-		if lSquare < rSquare {
-			answer = append(answer, lSquare)
-			l--
+		if lSqr > rSqr {
+			ans[i] = lSqr
+			l++
 		} else {
-			answer = append(answer, rSquare)
-			r++
+			ans[i] = rSqr
+			r--
 		}
 	}
 
-	for l >= 0 {
-		answer = append(answer, nums[l]*nums[l])
-		l--
-	}
-	for r < len(nums) {
-		answer = append(answer, nums[r]*nums[r])
-		r++
-	}
-
-	return answer
+	return ans
 }
 
 func main() {
 	// nums := []int{-4, -1, 0, 3, 10}
-	// nums := []int{-7, -3, 2, 3, 11}
-	nums := []int{-1}
+	nums := []int{-7, -3, 2, 3, 11}
+	// nums := []int{-1}
 	fmt.Println(sortedSquares(nums))
 }
